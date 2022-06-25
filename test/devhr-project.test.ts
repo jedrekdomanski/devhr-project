a624b0e8c3401db99135b506f3da8327af7b372b
jedrekdomanski <jedrek.domanski@gmail.com>
2022-06-25 18:20:55 +0200

    Initial commit

---
+++ test/devhr-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevhrProject from '../lib/devhr-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/devhr-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevhrProject.DevhrProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
