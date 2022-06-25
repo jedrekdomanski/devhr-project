03e8570bae9cf82949c26b5941123fd8dfed24db
jedrekdomanski <jedrek.domanski@gmail.com>
2022-06-25 20:19:12 +0200

    Dependencies cleanup

--- lib/devhr-project-stack.ts
+++ lib/devhr-project-stack.ts
@@ -1,39 +1,39 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk/aws-s3';
-import * as lambda from 'aws-cdk/aws-lambda';
-import * as dynamodb from 'aws-cdk/aws-dynamodb';
-import { Duration } from 'aws-cdk/core';
-import * as iam from 'aws-cdk/aws-iam';
-import * as event_sources from 'aws-cdk/aws-lambda-event-sources';
+import * as cdk from '@aws-cdk/core';
+import { Bucket, EventType } from '@aws-cdk/aws-s3';
+import { Function, Code, Runtime } from '@aws-cdk/aws-lambda';
+import { Table, AttributeType} from '@aws-cdk/aws-dynamodb';
+import { Duration } from '@aws-cdk/core';
+import { PolicyStatement, Effect } from '@aws-cdk/aws-iam';
+import { S3EventSource } from '@aws-cdk/aws-lambda-event-sources';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 const imageBucketName = 'cdk-rekn-imagebucket';
 
-export class DevhrProjectStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class DevhrProjectStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // =========================================================
     // Image Bucket
     // =========================================================
-    const imageBucket = new s3.Bucket(this, 'imageBucketName')
+    const imageBucket = new Bucket(this, 'imageBucketName');
     new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
 
     // =========================================================
     // Amazon DynamoDB table for storing image labels
     // =========================================================
-    const table = new.dynamodb.Table(this, 'ImageLabels', {
-      partitionKey: { name: 'image', type: dynamodbAttributeType.STRING }
+    const table = new Table(this, 'ImageLabels', {
+      partitionKey: { name: 'image', type: AttributeType.STRING }
     });
-    new cdk.CfnOutput(this, 'ddbTable', { value: table. })
+    new cdk.CfnOutput(this, 'ddbTable', { value: table.tableName });
 
     // =========================================================
     // AWS Lambda Function; compute for serverless microservice
     // =========================================================
-    const rekFn = new.lambda.Function(this, 'rekognitionFunction', {
-      code: lambda.Code.fromAsset('rekognitionlambda'),
-      runtime: lambda.Runtime.PYTHON_3_7,
+    const rekFn = new Function(this, 'rekognitionFunction', {
+      code: Code.fromAsset('rekognitionlambda'),
+      runtime: Runtime.PYTHON_3_7,
       handler: 'index.handler',
       timeout: Duration.seconds(30),
       memorySize: 1024,
@@ -42,11 +42,12 @@ export class DevhrProjectStack extends Stack {
         'BUCKET': imageBucket.bucketName
       }
     });
-    rekFn.addEventSource(new event_sources.S3EventSource(imageBucket, { events: [S3.EventType.OBJECT_CREATED]}))
+
+    rekFn.addEventSource(new S3EventSource(imageBucket, { events: [EventType.OBJECT_CREATED] }));
     imageBucket.grantRead(rekFn);
     table.grantWriteData(rekFn);
-    rekFn.addToRolePolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
+    rekFn.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
       actions: ['rekognition:DetectLabels'],
       resources: ['*']
     }));
