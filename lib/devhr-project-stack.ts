b42173b85e7691019bece636b266f48a6302010d
jedrekdomanski <jedrek.domanski@gmail.com>
2022-06-26 11:43:44 +0200

    Create services for image resizing

--- lib/devhr-project-stack.ts
+++ lib/devhr-project-stack.ts
@@ -1,24 +1,40 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket, EventType } from '@aws-cdk/aws-s3';
-import { Function, Code, Runtime } from '@aws-cdk/aws-lambda';
+import { Function, Code, Runtime, LayerVersion } from '@aws-cdk/aws-lambda';
 import { Table, AttributeType} from '@aws-cdk/aws-dynamodb';
 import { Duration } from '@aws-cdk/core';
 import { PolicyStatement, Effect } from '@aws-cdk/aws-iam';
 import { S3EventSource } from '@aws-cdk/aws-lambda-event-sources';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-const imageBucketName = 'cdk-rekn-imagebucket';
-
 export class DevhrProjectStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const imageBucketName = 'cdk-rekn-imagebucket';
+    const resizedBucketName = imageBucketName + '-resized'
+
     // =========================================================
     // Image Bucket
     // =========================================================
-    const imageBucket = new Bucket(this, 'imageBucketName');
+    const imageBucket = new Bucket(this, imageBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
     new cdk.CfnOutput(this, 'imageBucket', { value: imageBucket.bucketName });
+
+    // =========================================================
+    // Thumbnail Image bucket
+    // =========================================================
+    const resizedBucket = new Bucket(this, resizedBucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+    new cdk.CfnOutput(this, 'resizedBucket', { value: resizedBucket.bucketName });
+
+    const rekLambdaLayer = new LayerVersion(this, 'rekLambdaLayer', {
+      code: Code.fromAsset('reklayer'),
+      compatibleRuntimes: [Runtime.PYTHON_3_7],
+      license: 'Apache-2.0',
+      description: 'A Lambda Layer to enable to PIL library in Rekognition Lambda'
+    });
 
     // =========================================================
     // Amazon DynamoDB table for storing image labels
@@ -37,15 +53,35 @@ export class DevhrProjectStack extends cdk.Stack {
       handler: 'index.handler',
       timeout: Duration.seconds(30),
       memorySize: 1024,
+      layers: [rekLambdaLayer],
       environment: {
         'TABLE': table.tableName,
-        'BUCKET': imageBucket.bucketName
+        'BUCKET': imageBucket.bucketName,
+        'THUMBBUCKET': resizedBucket.bucketName
       }
     });
 
-    rekFn.addEventSource(new S3EventSource(imageBucket, { events: [EventType.OBJECT_CREATED] }));
     imageBucket.grantRead(rekFn);
+    resizedBucket.grantPut(rekFn);
     table.grantWriteData(rekFn);
+
+    const serviceFn = new Function(this, 'serviceFunction', {
+      code: Code.fromAsset('servicelambda'),
+      runtime: Runtime.PYTHON_3_7,
+      handler: 'index.handler',
+      environment: {
+        'TABLE': table.tableName,
+        'BUCKET': imageBucket.bucketName,
+        'RESIZEDBUCKET': resizedBucket.bucketName
+      }
+    });
+
+    imageBucket.grantWrite(serviceFn);
+    resizedBucket.grantWrite(serviceFn);
+    table.grantReadWriteData(serviceFn);
+
+    rekFn.addEventSource(new S3EventSource(imageBucket, { events: [EventType.OBJECT_CREATED] }));
+
     rekFn.addToRolePolicy(new PolicyStatement({
       effect: Effect.ALLOW,
       actions: ['rekognition:DetectLabels'],
